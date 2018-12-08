f90b2eb1a3234b3643afa46a27287a48478440a6
Hansimov <591172499@qq.com>
2018-12-08 22:45:57 +0800

    + Update documentation

---
+++ doc/scatool/html2pdf.py
@@ -0,0 +1,8 @@
+import pdfkit
+
+html_list = [  './html/doc_01_installation.html',
+               './html/doc_02_gui.html',
+               './html/doc_03_lib.html',
+               './html/doc_04_support.html'];
+
+pdfkit.from_file(html_list,'scatool_doc.pdf',options=options)
